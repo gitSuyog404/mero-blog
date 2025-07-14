e0429130b9324779e3901831e6da86fc2061e088
gitSuyog <codesuyog0@gmail.com>
2025-07-14 20:38:36 +0545

    get all users done

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -24,6 +24,8 @@ const config = {
   ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as ms.StringValue,
   REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as ms.StringValue,
   WHITELIST_ADMINS_MAIL: ['admintest@gmail.com'],
+  defaultResLimit: 20,
+  defaultResOffset: 0,
 };
 
 export default config;
--- backend/src/routes/v1/user.ts
+++ backend/src/routes/v1/user.ts
@@ -17,6 +17,8 @@ import authorize from '@/middlewares/authorize';
  */
 import getCurrentUser from '@/controllers/v1/user/getCurrentUser';
 import updateCurrentUser from '@/controllers/v1/user/updateCurrentUser';
+import deleteCurrentUser from '@/controllers/v1/user/deleteCurrentUser';
+import getAllUser from '@/controllers/v1/user/getAllUser';
 /**
  * Models
  *
@@ -60,9 +62,43 @@ router.put(
         throw Error('This email is already in use');
       }
     }),
-    body('password').optional().isLength({min:8}).withMessage('Password must be at least 8 characters long'),
-    body('first_name').optional().isLength({max: 20})
+  body('password')
+    .optional()
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long'),
+  body('first_name')
+    .optional()
+    .isLength({ max: 20 })
+    .withMessage('First name must be less than 20 characters'),
+  body('last_name')
+    .optional()
+    .isLength({ max: 20 })
+    .withMessage('Last name must be less than 20 characters'),
+  body(['website', 'facebook', 'instagram', 'linkedin', 'x', 'youtube'])
+    .optional()
+    .isURL()
+    .withMessage('Invalid URL')
+    .isLength({ max: 100 })
+    .withMessage('URL must be less than 100 characters'),
   validationError,
   updateCurrentUser,
 );
+
+router.delete(
+  '/current',
+  authenticate,
+  authorize(['admin', 'user']),
+  query('limit')
+    .optional()
+    .isInt({ min: 1, max: 50 })
+    .withMessage('Limit must be between 1 to 50'),
+  query('offset')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Offset must be a positive integer'),
+  validationError,
+  deleteCurrentUser,
+);
+
+router.get('/', authenticate, authorize(['admin']), getAllUser);
 export default router;
